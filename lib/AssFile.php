26997204b95459c0795b25369d7e9fb5ded7fe02
Naouak <tardiel@gmail.com>
2012-12-10 23:49:56 +0100

    Header Information parsing and validation. Should be right according to ass file spec.

--- lib/AssFile.php
+++ lib/AssFile.php
@@ -10,6 +10,29 @@ namespace LibPHPAss;
 class AssFile {
     private $type = null;
 
+    static private $headParamsAccepted = array(
+        "Title",
+        "Original Script",
+        "Original Translation",
+        "Original Editing",
+        "Original Timing",
+        "Synch Point",
+        "Script Updated By",
+        "Update Details",
+        "Script Type",
+        "Collisions",
+        "PlayResX",
+        "PlayResY",
+        "PlayDepth",
+        "Timer",
+        "Wrap Style"
+    );
+
+    private $head = array();
+
+    /**
+     * @param $content string Content of the ass file.
+     */
     private function __construct($content){
         //Remove any windows line returns
         $content = strstr("\r\n",$content);
@@ -23,10 +46,14 @@ class AssFile {
         }
 
         $i = 1;
+        $j = $i;
         //While in header
         while($i < sizeof($lines) && $lines[$i][0] != "["){
             $i++;
         }
+
+        $header = array_slice($lines,$j,$i-$j);
+        $this->parseHeader($header);
 
         //Styles (and ass or ssa determination)
         if(strtolower(trim($lines[$i])) == "[v4 styles]"){
@@ -80,5 +107,44 @@ class AssFile {
         }
 
         return self::loadFromString(file_get_contents($file));
+    }
+
+    /**
+     * @param $param String Name of the parameter to send
+     * @param $value String Value of the parameter
+     * @return bool
+     */
+    private function setHeaderInformation($param,$value){
+        if(array_search($param,self::$headParamsAccepted) === false){
+            return false;
+        }
+
+        //Let's manage parameters that have only a set of predefined values
+        if($param == "Script Type" && ($value != "V4.00" && $value != "V4.00+")){
+            return false;
+        } else if ($param == "Collisions" && ($value != "Normal" && $value != "Reverse")) {
+            return false;
+        } else if (($param == "PlayResX" || $param == "PlayResY" || $param == "PlayDepth" || $param == "Timer") && (!is_numeric($value))) {
+            return false;
+        } else if ($param == "Wrap Style" && (!is_numeric($value) || $value < 0 || $value > 3)) {
+            return false;
+        }
+
+        $this->head[$param] = $value;
+        return true;
+    }
+
+    /**
+     * @param $header Array Strings that forms the header of the file.
+     */
+    private function parseHeader($header)
+    {
+        foreach ($header as $line) {
+            $data = explode(":",$line,2);
+            if(sizeof($data) == 2){
+                $this->setHeaderInformation($data[0],$data[1]);
+            }
+        }
+
     }
 }
